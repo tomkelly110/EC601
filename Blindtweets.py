60a2a853424eaaae3cd1e666e90695178434c26e
tomkelly110 <43146563+tomkelly110@users.noreply.github.com>
2018-09-12 13:59:40 -0400

    psuedocode
    A general idea of the structure of the coding to come.

---
+++ Blindtweets.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import tweepy
+import json
+
+con_key = "Consumer key"
+con_sec = "Consumer secret key"
+acc_key = "Access key"
+acc_sec = "Access secret key"
+
+
+if __name__ == '__main__':
+	print("Hello and welcome to this version of MiniProject 1.\nThis module was coded by Thomas Kelly.\nPlease enter your consumer key.")
+	#get codes
+	#get twitter name
+	#digest name
+	#while loop if they want to look at other names
+
+
+
+
+
+	#def digest(username):
+	#authorize to twitter
+	#put tweets in list
+	#seperate out images
+	#put images into video
+	#put video into vision
+	#output vision text
