58db881d81344a12d658ab8f5464f3bb04627780
tomkelly110 <43146563+tomkelly110@users.noreply.github.com>
2018-09-12 16:02:47 -0400

    Method and while loop
    Added a while loop that intakes the twitter handles and finds out if the user wants to try another handle after hte first. This loops calls a method that will handle the primary functionality.

--- Blindtweets.py
+++ Blindtweets.py
@@ -5,25 +5,27 @@ con_key = "Consumer key"
 con_sec = "Consumer secret key"
 acc_key = "Access key"
 acc_sec = "Access secret key"
-if __name__ == '__main__':
-	con_key = input("Hello and welcome to this version of MiniProject 1.\nThis module was coded by Thomas Kelly.\nPlease enter your Consumer Key.\n")
-	con_sec = input("Please enter your Consumer Secret Key\n")
-	acc_key = input("Please enter your Access Key\n")
-	acc_sec = input("Please enter your Access Secret Key\n")
-	print(con_key+con_sec+acc_key+acc_sec)
-	#get codes
-	#get twitter name
-	#digest name
-	#while loop if they want to look at other names
 
-
-
-
-
-	#def digest(username):
+def digest(username):
+	print(username)
 	#authorize to twitter
 	#put tweets in list
 	#seperate out images
 	#put images into video
 	#put video into vision
 	#output vision text
+
+
+if __name__ == '__main__':
+	con_key = input("Hello and welcome to this version of MiniProject 1.\nThis module was coded by Thomas Kelly.\nPlease enter your Consumer Key.\n")
+	con_sec = input("Please enter your Consumer Secret Key\n")
+	acc_key = input("Please enter your Access Key\n")
+	acc_sec = input("Please enter your Access Secret Key\n")
+	print(con_key+con_sec+acc_key+acc_sec)
+	flag = 1
+	targetname = "@BarackObama"
+	while flag > 0:
+		targetname = input("Please enter the desired twitter handle, including the @ symbol\n")
+		digest(targetname)
+		if input("Would you like to see any other twitter handles? (y/n)\n") != "y":
+			flag = 0
